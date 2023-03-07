5c67861b7013adc2eaaaa0b9369f315a5c40cabf
Hegeldirkk <hegeldirkk@gmail.com>
2023-03-07 18:36:05 +0000

    script git writting - redis js ok

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,5 @@
-import { createClient, } from 'redis';
-import { promisify, } from 'util';
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
